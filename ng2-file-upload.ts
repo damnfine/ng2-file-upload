40989decc24db082d9307d567b1c13fc70d3a9ea
Ben Jackson <benjackson84@gmail.com>
2016-12-30 10:46:10 +0000

    locked angular version and exporting FileItem and FileLikeObject

--- ng2-file-upload.ts
+++ ng2-file-upload.ts
@@ -1,5 +1,7 @@
 export * from  './components/file-upload/file-select.directive';
 export * from  './components/file-upload/file-drop.directive';
 export * from  './components/file-upload/file-uploader.class';
+export * from  './components/file-upload/file-item.class';
+export * from  './components/file-upload/file-like-object.class';
 
 export { FileUploadModule } from './components/file-upload/file-upload.module';
